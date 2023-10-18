091bd4d656b04a82969d76cd05d6594bba57ba41
francoviscarra <98550346+francoviscarra@users.noreply.github.com>
2023-10-18 20:29:56 +0100

    Update gui.py

--- pyCRC_plot/gui.py
+++ pyCRC_plot/gui.py
@@ -7,9 +7,9 @@ import io
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from import pyCRC_plot import CRC
+from pyCRC_plot import CRC
 import os
-from import pyCRC_plot import show_results
+from pyCRC_plot import show_results
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
